e316e9a52a4c2431458c23f48d602dc923885474
victordoshenko <victor.doshenko@gmail.com>
2019-11-04 20:14:42 +0300

    enemy done
    first enemy done, not fall from platform

--- Assets/Scripts/walkingEnemy.cs
+++ Assets/Scripts/walkingEnemy.cs
@@ -6,15 +6,15 @@ public class walkingEnemy : MonoBehaviour
     public float speed = 2f;
     float direction = -1f;
     private Rigidbody2D pRigidBody;
-    // Use this for initialization
 
-    public LineRenderer l;
-    int len = 2;
+    //public LineRenderer l;
+    //int len = 2;
 
     void Start()
     {
         pRigidBody = GetComponent<Rigidbody2D>();
-        l.positionCount = 2;
+        //l.positionCount = 2;
+        //l.useWorldSpace = true;
     }
 
     void FixedUpdate()
@@ -23,45 +23,19 @@ public class walkingEnemy : MonoBehaviour
         transform.localScale = new Vector3(direction, 1, 1);
 
         LayerMask maskGround = LayerMask.GetMask("Ground");
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(direction * 1.5f, 0), transform.position + new Vector3(direction * 1.5f, -direction * 1.5f), 1.5f, maskGround);
-        l.SetPosition(0, transform.position + new Vector3(direction * 1.5f, 0));
-        l.SetPosition(1, transform.position + new Vector3(direction * 1.5f, -1.5f));
-        if (hit.transform == null)
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + direction * 0.5f, transform.position.y), Vector2.down, 1f, maskGround);
+
+        //l.SetPosition(0, new Vector2(transform.position.x + direction * 1.5f, transform.position.y));
+        //l.SetPosition(1, new Vector2(transform.position.x + direction * 1.5f, transform.position.y - 1.5f));
+
+        if (hit.collider == null)
             direction *= -1f;
     }
 
-/*
-    // Update is called once per frame
-    void Update()
-    {
-
-        //Vector2 mousePos = transform.position; //Input.mousePosition;
-        //Vector3 pos3d = Camera.main.ScreenToWorldPoint(new Vector3(transform.position.x, transform.position.y, Camera.main.nearClipPlane));
-        //l.positionCount = 2;
-        //l.SetPosition(0, transform.position + new Vector3(direction * 1.5f, 0));
-        //l.SetPosition(1, transform.position + new Vector3(direction * 1.5f, 0) + new Vector3(0, -1.5f));
-        //len++;
-    }
-*/
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Wall")
             direction *= -1f;
     }
-/*
-    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 1f)
-    {
-        GameObject myLine = new GameObject();
-        myLine.transform.position = start;
-        myLine.AddComponent<LineRenderer>();
-        LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-        lr.SetColors(color, color);
-        lr.SetWidth(0.1f, 0.1f);
-        lr.SetPosition(0, start);
-        lr.SetPosition(1, end);
-        GameObject.Destroy(myLine, duration);
-    }
-*/
 }
