e6636e9a7393ca9b6d6d7dc8c1642ee5ef6471ac
victordoshenko <victor.doshenko@gmail.com>
2019-12-05 16:17:22 +0300

    Flying letters completed

--- Assets/Scripts/meshRendererForeground.cs
+++ Assets/Scripts/meshRendererForeground.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class meshRendererForeground : MonoBehaviour
 {
+    float speed = 7f;
+    float height = 0.2f;
+    float y_start = 0f;
+
     void Start()
     {
         this.gameObject.GetComponent<MeshRenderer>().sortingLayerName = "Foreground";
         this.gameObject.GetComponent<MeshRenderer>().sortingOrder = 50;
+        y_start = transform.position.y;
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float newY = Mathf.Sin(Time.time * speed);
+        transform.position = new Vector3(pos.x, y_start + newY * height, pos.z);
     }
 }
