d70139beddc7eaae04fd5739e1a1fafd5d4cc457
victordoshenko <victor.doshenko@gmail.com>
2019-12-27 15:23:28 +0300

    Return button from Intro level
    +resolution fixes for iphones

---
+++ Assets/Scripts/ReturnButton_Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnButton_Script : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("GameMenu");
+    }
+}
